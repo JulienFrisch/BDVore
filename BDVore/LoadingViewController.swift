910ee721068a54bf9ec18d07d6d510b548ff2b3d
Julien Frisch <julien_frisch@outlook.com>
2016-05-07 08:46:01 -0500

    Error Alert in LoadScreen if no internet connection

--- BDVore/LoadingViewController.swift
+++ BDVore/LoadingViewController.swift
@@ -11,17 +11,39 @@ import UIKit
 
 class LoadingViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        performSegueWithIdentifier("loadingOver", sender: nil)
-
+    //We use viewDidAppear instead of ViewDidLoad to make sure the UIAlertController will be properly displayed
+    override func viewDidAppear(animated: Bool) {
+        
+        super.viewDidAppear(animated)
+        
         // Do any additional setup after loading the view.
+        //we check that there is a network connection
+        if NetworkOperation.isConnectedToNetwork(){
+            performSegueWithIdentifier("loadingOver", sender: nil)
+        } else {
+            // Create the alert controller
+            let alertController = UIAlertController(title: "Erreur réseau", message: "BDVore n'a pas pu se connecter à internet!", preferredStyle: .Alert)
+            
+            // Create the action
+            let okAction = UIAlertAction(title: "Quitter l'application", style: UIAlertActionStyle.Default) {
+                UIAlertAction in
+                exit(0)
+            }
+            
+            // Add the actions
+            alertController.addAction(okAction)
+            
+            // Present the controller
+            self.presentViewController(alertController, animated: true, completion: nil)
+        }
     }
 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     
 
     /*
--- BDVore/NetworkOperation.swift
+++ BDVore/NetworkOperation.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import SystemConfiguration
 
 class NetworkOperation{
     
@@ -46,6 +47,25 @@ class NetworkOperation{
             }
         }
         dataTask.resume()
+    }
+    
+    /**
+     Check if connected to network
+    */
+    class func isConnectedToNetwork() -> Bool {
+        var zeroAddress = sockaddr_in()
+        zeroAddress.sin_len = UInt8(sizeofValue(zeroAddress))
+        zeroAddress.sin_family = sa_family_t(AF_INET)
+        let defaultRouteReachability = withUnsafePointer(&zeroAddress) {
+            SCNetworkReachabilityCreateWithAddress(nil, UnsafePointer($0))
+        }
+        var flags = SCNetworkReachabilityFlags()
+        if !SCNetworkReachabilityGetFlags(defaultRouteReachability!, &flags) {
+            return false
+        }
+        let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+        let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+        return (isReachable && !needsConnection)
     }
     
 }
